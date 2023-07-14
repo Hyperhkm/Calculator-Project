f3ac0b9aac25b302a10b2c416469c0c8ee0fdfd4
hyperhkm <lucasjamaico@gmail.com>
2023-07-14 19:35:32 -0300

    .

--- script.js
+++ script.js
@@ -4,19 +4,26 @@ const display = document.querySelector('#display');
 const acButton = document.querySelector('#ac');
 const equalButton = document.querySelector('#equals');
 const dotButton = document.querySelector('#dot');
+const signButton = document.querySelector('#sign');
 
 let result = 0,
     operation = 0,
     displayNumber = 0;
+    clear = function () {
+        result = 0;
+        operation = 0;
+        displayNumber = 0;
+    };
 
 acButton.onclick = () => clearEvent();
 
 function clearEvent() {
-    result = 0;
-    operation = 0;
-    displayNumber = 0;
-    display.innerHTML = result;
-}
+    clear();
+    display.innerHTML = 0;
+};
+function check (e) {
+    return displayNumber.toString().split('').indexOf(e)
+};
 
 document.addEventListener('DOMContentLoaded', function() {
     numbers.forEach(number => {
@@ -29,8 +36,8 @@ document.addEventListener('DOMContentLoaded', function() {
     operator.forEach(operator => {
         operator.addEventListener('click', function() {
             if(!Number(displayNumber)) {return}
-            if(eval(`${operation}${displayNumber}`) && operation !== 0) {
-                result = eval(`${operation}${displayNumber}`);
+            if(eval(`${operation+displayNumber}`) && operation !== 0) {
+                result = eval(`${operation+displayNumber}`);
                 displayNumber = result;
                 display.innerHTML = result;
             }
@@ -40,13 +47,27 @@ document.addEventListener('DOMContentLoaded', function() {
         })
     })
     equalButton.addEventListener('click', function() {
-        if (!eval(`${operation}${displayNumber}`)) {return}
-        result = eval(`${operation}${displayNumber}`);
+        if (!eval(`${operation+displayNumber}`)) {return}
+        result = eval(`${operation+displayNumber}`);
         display.innerHTML = result;
+        clear();
     })
     dotButton.addEventListener('click', function(){
-        if(!Number(displayNumber) || displayNumber.toString().split('').indexOf('.') > -1) {return}
+        if(!Number(displayNumber) || check('.') > -1) {return}
         displayNumber += '.';
+        display.innerHTML = displayNumber;
+    })
+    signButton.addEventListener('click', function(){
+        if(displayNumber == 0) {return};
+        if(check('-') > -1) {
+            displayNumber = Number(displayNumber.toString().split('').slice(1).join('')); return display.innerHTML = displayNumber;
+        }
+        (() => {
+            let newNumber = displayNumber;
+            newNumber = displayNumber.toString().split('');
+            newNumber.unshift('-');
+            displayNumber = Number(newNumber.join(''));
+        })();
         display.innerHTML = displayNumber;
     })
 })
